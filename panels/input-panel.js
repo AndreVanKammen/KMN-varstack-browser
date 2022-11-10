4f340679ab377f3c8894479b76be43ce1fa10a35
AndreVanKammen <andrevankammen@gmail.com>
2022-11-10 18:57:27 +0100

    Input panel for showing contents of record or structure

---
+++ panels/input-panel.js
@@ -0,0 +1,61 @@
+// Copyright by André van Kammen
+// Licensed under CC BY-NC-SA
+// https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+import PanelBase from '../../KMN-utils-browser/components/panel-base.js';
+import InputBuilder from '../components/input-builder.js';
+import { TableBuilder } from '../components/table-builder.js';
+import { addCSS, kmnClassName } from '../utils/html-utils.js';
+
+const cssStr = /*css*/`
+.${kmnClassName}.inputDiv {
+  height: 100%;
+}
+.${kmnClassName}.inputDiv td.isLabel {
+  width: 300px;
+}
+`
+
+const defaultOptions = {
+};
+class InputPanel extends PanelBase {
+  constructor(options) {
+    super(defaultOptions, options);
+
+    this.searchTable = options.searchTable;
+    this.input = null;
+  }
+
+  /**
+   * @param {HTMLElement} parentElement
+   */
+  initializeDOM(parentElement) {
+    super.initializeDOM(parentElement);
+    addCSS('table-panel',cssStr);
+
+    this.inputDiv = this.parentElement.$el({ cls: "inputDiv" });
+
+    this.input = new InputBuilder(
+      this.inputDiv,
+      {
+        ...{
+          showValues: true,
+          showSubRecords: true,
+          showTableValues: true
+        },
+        ...this.options
+      });
+
+    if (this.options.content) {
+      this.input.addRecord(this.options.content);
+    }
+  }
+
+  show() {
+    let result = super.show();
+    return result;
+  }
+}
+InputPanel.getTabName = () => 'INPUT';
+
+export default InputPanel;
